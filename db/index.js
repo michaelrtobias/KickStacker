12a447e45067cc4ceb6da5347ba6ee490d7ffdc5
Michael Tobias <michaelremitobias@gmail.com>
2020-11-11 01:59:24 -0700

    Connected postgress

---
+++ db/index.js
@@ -0,0 +1,14 @@
+const {Client} = require('pg');
+const {Sequelize} = require('sequelize')
+const dotenv = require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {logging: false});
+try {
+  sequelize.authenticate();
+  console.log('postgreSQL is connected!');
+} catch (error) {
+  throw err;
+}
+
+
+module.exports = sequelize;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config()
 const path = require('path');
+const db = require('../db/index.js');
+
 
 app.use(express.static(path.join(__dirname, '../public')))
 
