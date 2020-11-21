e60d4a9c9cd4c12c0c15bde3be8a60d3aba4fe49
Michael Tobias <michaelremitobias@gmail.com>
2020-11-20 18:01:11 -0700

    signin is rendering with static user list

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 
-function SignIn() {
-
+function SignIn(props) {
   return (
+    <div>
     <div>SignIn is rendered</div>
+    <select name="userPicker" id="userPicker" placeholder="Pick A User">
+      {props.users.map((user) =>
+        <option value={user.id}>{user.firstName} {user.lastName}</option>
+      )}
+    </select>
+    </div>
   )
 }
 
