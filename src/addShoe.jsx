2e9c4ecd1cd9334b0084a83cf55ca197d5bd3753
Michael Tobias <michaelremitobias@gmail.com>
2020-11-23 16:35:22 -0700

    hooks for form are finished

--- src/addShoe.jsx
+++ src/addShoe.jsx
@@ -13,7 +13,18 @@ function AddShoe(props) {
   const [shoeNickname, setShoeNickname] = useState('');
   const [shoeColor, setShoeColor] = useState('');
   const [shoeSize, setShoeSize] = useState(null);
-
+  const [sizeTypes, setSizeTypes] = useState([]);
+  const [sizeType, setSizeType] = useState(null);
+  const [boxStatus, setBoxStatus] = useState(null);
+  const [wears, setWears] = useState(null);
+  const [purchasePrice, setPurchasePrice] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [recieptStatus, setRecieptStatus] = useState(null);
+  const [collaborator, setCollaborator] = useState(null);
+  const [types, setTypes] = useState([]);
+  const [shoeType, setShoeType] = useState(null);
+  const [cuts, setCut] = useState([]);
+  const [shoeCut, setShoeCut] = useState(null);
 
   const getAllBrands = () => {
     axios('/brands')
@@ -40,6 +51,12 @@ function AddShoe(props) {
     .then(models => setModels(models))
     .catch(err => console.log(err))
   }
+  const getSizeTypes = () => {
+    axios(`/sizeTypes`)
+    .then(res => res.data)
+    .then(sizeType => setSizeTypes(sizeType))
+    .catch(err => console.log(err))
+  }
 
   const handleCollectionChange = (e) => {
     setCollectionId(e.target.value)
@@ -52,7 +69,7 @@ function AddShoe(props) {
 
   useEffect(() => {
     getAllBrands()
-
+    getSizeTypes()
   }, [])
 
   return (
@@ -86,19 +103,38 @@ function AddShoe(props) {
         </select>
       </div>
       <div>
+        <label>Name on Box:</label>
         <input placeholder="Enter Name on Box" onChange={e => setShoeName(e.target.value)}></input>
       </div>
       <div>
+        <label>Shoe Nickname:</label>
         <input placeholder="Enter Nickname" onChange={e => setShoeNickname(e.target.value)}></input>
       </div>
       <div>
+        <label>Style Code:
+        </label>
         <input placeholder="Enter Style Code" onChange={e => setStyleCode(e.target.value)}></input>
       </div>
       <div>
+        <label>Size:</label>
         <input placeholder="Enter Size" onChange={e => setShoeSize(e.target.value)}></input>
+        <select onChange={e => setSizeType(e.target.value)}>
+        <option selected>Select a Size Type</option>
+        {sizeTypes.map(type =>
+            <option value={type.id}>{type.sizeType}</option>
+            )}
+        </select>
       </div>
       <div>
+        <label>Color:</label>
         <input placeholder="Enter Color" onChange={e => setShoeColor(e.target.value)}></input>
+      </div>
+      <div>
+        <label>Is there a box? :  </label>
+        <label for="boxstatus">True</label>
+        <input  type="radio" value="true" name="boxstatus" onChange={e => setBoxStatus(e.target.value)}></input>
+        <label for="boxstatus">False</label>
+        <input  type="radio" value="false" name="boxstatus" onChange={e => setBoxStatus(e.target.value)}></input>
       </div>
     </form>
   )
