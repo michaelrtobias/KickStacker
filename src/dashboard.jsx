96f44e9ebd49475575429863b61df80258759fc4
Michael Tobias <michaelremitobias@gmail.com>
2020-12-01 14:23:46 -0700

    Added change user btn

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -13,7 +13,10 @@ function Dashboard(props) {
   return (
     <div>
       <div>{props.user.firstName}'s Dashboard</div>
-      <button onClick={() => handleClickAddShoe()}>Add A Shoe</button>
+      <div>
+        <button onClick={() => handleClickAddShoe()}>Add A Shoe</button>
+        <button onClick={() => props.setView("signin")}>Change User</button>
+      </div>
       <Collection userSneakers={props.userSneakers} />
     </div>
   );
