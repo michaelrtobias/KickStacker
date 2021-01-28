973b7fab12e22a466f5e149249b0a3be70398a8f
Michael Tobias <michaelremitobias@gmail.com>
2021-01-28 13:20:03 -0700

    Bogie Stacker

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ function App() {
 
   return (
     <Body>
-      <h1>Bogie Stacker</h1>
+      <h1>Iced Tea Stacker</h1>
       {view === "signin" ? null : (
         <Header>
           <HoverWrapper>
