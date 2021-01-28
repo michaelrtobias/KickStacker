73d8dab6b592801517cb9f28bae70fa9f583d9d4
Michael Tobias <michaelremitobias@gmail.com>
2021-01-28 15:04:11 -0700

    Reverted back to shoestacker

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ function App() {
 
   return (
     <Body>
-      <h1>Iced Tea Stacker</h1>
+      <h1>Shoes Stacker</h1>
       {view === "signin" ? null : (
         <Header>
           <HoverWrapper>
