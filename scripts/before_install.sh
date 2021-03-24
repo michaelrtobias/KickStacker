2695bf63d1f1c42fa2a6c27c7feb0f5259304691
Michael Tobias <michaelremitobias@gmail.com>
2021-03-23 17:00:45 -0700

    update code deploy scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/KickStacker
+
+yum -y update
+
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 yum install nodejs npm
---
+++ scripts/stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/
+cd /home/ec2-user/KickStacker
+pm2 stop www || true
